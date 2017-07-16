51c9016aafd04be6e9ec6b25acc69b6eadc1f059
pierreolivierroy <roy.pierre.o@gmail.com>
2017-07-16 14:23:48 -0400

    Project scaffolding

---
+++ src/main/java/com/poc/poccart/PocCartApplication.java
@@ -0,0 +1,12 @@
+package com.poc.poccart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocCartApplication.class, args);
+	}
+}
---
+++ src/test/java/com/poc/poccart/PocCartApplicationTests.java
@@ -0,0 +1,16 @@
+package com.poc.poccart;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PocCartApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
